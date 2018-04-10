375a0a5d9c8b59bf6eed436d54bfbd5292c3cf69
Paul den Dulk <pauldendulk@gmail.com>
2018-04-10 21:12:33 +0200

    Adding bing api key variable.

--- Samples/Mapsui.Samples.Common/Maps/BingSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/BingSample.cs
@@ -8,7 +8,11 @@ namespace Mapsui.Samples.Common.Maps
         public static Map CreateMap()
         {
             var map = new Map();
-            map.Layers.Add(new TileLayer(KnownTileSources.Create(KnownTileSource.BingAerial)) {Name = "Bing Aerial"});
+            var apiKey = "Enter your api key here"; // Contact Microsoft about how to use this
+            map.Layers.Add(new TileLayer(KnownTileSources.Create(KnownTileSource.BingAerial, apiKey))
+            {
+                Name = "Bing Aerial"
+            });
             return map;
         }
     }
